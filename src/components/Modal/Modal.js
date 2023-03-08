c4c5bc23ce767559ed18639f7a11fb82d5397002
Sean Kozen <sgkozen@gmail.com>
2023-03-08 11:40:43 +0900

    Final

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React from "react";
+import CSSTransition from "react-transition-group/CSSTransition";
 
-import './Modal.css';
+import "./Modal.css";
 
-const modal = (props) => (
-    <div className="Modal">
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
+const modal = (props) => {
+  return (
+    //timeout is best the same time as the css timing
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed'
+        //appear:
+        //appearActive:
+      }}
+    >
+      <div className="Modal">
         <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
-);
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
+  );
+};
 
 export default modal;
